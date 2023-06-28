7b6bd3185d7427949036fcf2bf74e512ec6052f2
Yordan Stankov <78852840+YordanStankov@users.noreply.github.com>
2023-06-28 17:40:04 +0300

    Add files via upload

---
+++ NewsEditor/ConsoleApp215/News.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Newspaper
+{
+    public class News
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Writter { get; set; }
+
+        public News (string title, string text, string writter)
+        {
+            Title = title;
+            Text = text;
+            Writter = writter;
+        }
+        public void Edit(string text)
+        {
+            Text = text;
+        }
+        public void Rename(string title)
+        {
+            Title = title;
+            
+        }
+        public void ChangeWritter(string writter)
+        {
+            Writter = writter;
+        }
+        public override string ToString()
+        {
+            return $"{Title} ‐ {Text}: {Writter}";
+        }
+
+    }
+}
---
+++ NewsEditor/ConsoleApp215/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; 
+
+namespace Newspaper
+{
+   public class Startup
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ").ToArray();
+            int n = int.Parse(Console.ReadLine());
+            News article = new News(input[0], input[1], input[2]);
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split(": ").ToArray();
+                switch (command[0])
+                {
+                    case "Edit":
+                        {
+                            article.Edit(command[1]);
+                            break;
+                        }
+                    case "ChangeWritter":
+                        {
+                            article.ChangeWritter(command[1]);
+                            break;
+                        }
+                    case "Rename":
+                        {
+                            article.Rename(command[1]);
+                            break;
+                        }
+                    case "Override":
+                        {
+                            Console.WriteLine($"{input[0]} - {input[1]}: {input[2]}");
+                            break;
+                        }
+                }
+            }
+            Console.WriteLine(article.ToString());
+            }
+
+        }
+    }
+
